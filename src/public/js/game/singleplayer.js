b0a015e361e75591f319aaf82201b2c6b1b45bf0
Matěj Kubíček <kubicekm23@spstrutnov.cz>
2026-02-20 19:18:10 +0100

    Přidání stránky pro singleplayer se základním zobrazením dat a funkcí pro zjištění nových
    Ta funkce se zatím nepoužívá.

---
+++ src/public/js/game/singleplayer.js
@@ -0,0 +1,20 @@
+async function updateGameState() {
+    const dataFromServer = await fetch('/api/game/state').then(res => res.json());
+
+    const playerId = document.getElementById('player-id').textContent;
+    const opponentName = 'Chudý starec';
+    const playerScore = dataFromServer.p1_score;
+    const opponentScore = dataFromServer.p2_score;
+    const currentRoll = dataFromServer.turn_score > 0 ? JSON.parse(dataFromServer.last_roll) : [];
+
+    document.getElementById('opponent-name').textContent = opponentName;
+    document.getElementById('player-score').textContent = playerScore;
+    document.getElementById('opponent-score').textContent = opponentScore;
+    document.getElementById('current-roll').textContent = currentRoll.join(', ');
+
+    if (dataFromServer.currentPlayerTurn == playerId) {
+        document.getElementById('roll-dice').disabled = false;
+    } else {
+        document.getElementById('roll-dice').disabled = true;
+    }
+}
---
+++ src/routes/gameRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const gameController = require('../controllers/gameController');
+
+router.get('/singleplayer', gameController.getSingleplayer);
+
+module.exports = router;
