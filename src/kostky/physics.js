2c6b35d83dbcfc53f8b8407bf43a5c6daa583725
Karel Čapek <capekk23@spstrutnov.cz>
2026-03-20 19:25:01 +0100

    Create physics.js

---
+++ src/kostky/physics.js
@@ -0,0 +1,18 @@
+const g = 9.81
+const rest = 0.3
+const friction = 0.95
+const angDamp = 0.998
+const numDice = 6
+
+
+let DiceState = {pos: [x,y,z], vel: [vx,vy,vz], rot: [rx,ry,rz], angVel: [wx,wy,wz]}
+
+
+function loop()
+{
+  //loop over dice
+  //apply downward velocity
+  //clamp it
+
+  //collisions?
+}
