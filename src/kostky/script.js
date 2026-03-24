d3e813e5523234adce79d0fd5608f1b623a0901a
Karel Čapek <capekk23@spstrutnov.cz>
2026-03-24 08:20:49 +0100

    Update script.js

--- src/kostky/script.js
+++ src/kostky/script.js
@@ -40,7 +40,7 @@ const vsSource = `
       highp vec3 directionalLightColor = vec3(1, 1, 1);
       highp vec3 directionalVector = normalize(vec3(0.85, 0.8, 0.75));
 
-      highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
+      highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 0.0);
 
       highp float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0);
       vLighting = ambientLight + (directionalLightColor * directional);
